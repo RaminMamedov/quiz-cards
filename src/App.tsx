95677b635aa7bf14e4e5eb4ccb4de81d834ae203
Ramin Mamedov <mamedov10rm@mail.ru>
2024-04-24 23:42:23 +0400

    feat: fix project2

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import { UserDropDown } from '@/components/layout'
+import { BrowserRouter } from 'react-router-dom'
+
+import Logo from '@/assets/icons/Logo'
+import { UserDropDown } from '@/components/layout/UserDropDown/UserDropDown'
+import { Header } from '@/components/ui/Header/Header'
 
 export function App() {
   return (
-    <div style={{ margin: ' 0 auto', maxWidth: '300px' }}>
-      {/*<DropDownMenu trigger={<Avatar userName={'Ramin'} />} />*/}
-      {/*<div>*/}
-      {/*  <DropDownMenu trigger={<MoreVerticalOutline />} />*/}
-      {/*</div>*/}
-      <UserDropDown email={'frontend-dev@gmail.com'} onLogout={() => {}} userName={'Ramin'} />
-    </div>
+    <BrowserRouter>
+      <Header email={'frontend-dev@gmail.com'} isLoggedIn logout={() => {}} name={'Ramin'} />
+      <Logo height={'36px'} width={'157px'} />
+      <div style={{ margin: ' 0 auto', maxWidth: '300px' }}>
+        {/*<DropDownMenu trigger={<Avatar userName={'Ramin'} />} />*/}
+        {/*<div>*/}
+        {/*  <DropDownMenu trigger={<MoreVerticalOutline />} />*/}
+        {/*</div>*/}
+        <UserDropDown email={'frontend-dev@gmail.com'} logout={() => {}} name={'Ramin'} />
+      </div>
+    </BrowserRouter>
   )
 }
