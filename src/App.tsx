169f2ac8e93b236565aa69ac87c908761d05595f
Ramin Mamedov <mamedov10rm@mail.ru>
2024-04-24 23:50:53 +0400

    feat: fix project3

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter } from 'react-router-dom'
 
 import Logo from '@/assets/icons/Logo'
-import { UserDropDown } from '@/components/layout/UserDropDown/UserDropDown'
+import { UserDropDown } from '@/components/layout/UserDropDown'
 import { Header } from '@/components/ui/Header/Header'
 
 export function App() {
@@ -10,10 +10,6 @@ export function App() {
       <Header email={'frontend-dev@gmail.com'} isLoggedIn logout={() => {}} name={'Ramin'} />
       <Logo height={'36px'} width={'157px'} />
       <div style={{ margin: ' 0 auto', maxWidth: '300px' }}>
-        {/*<DropDownMenu trigger={<Avatar userName={'Ramin'} />} />*/}
-        {/*<div>*/}
-        {/*  <DropDownMenu trigger={<MoreVerticalOutline />} />*/}
-        {/*</div>*/}
         <UserDropDown email={'frontend-dev@gmail.com'} logout={() => {}} name={'Ramin'} />
       </div>
     </BrowserRouter>
