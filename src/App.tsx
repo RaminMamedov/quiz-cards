35d4869e7ecee683c048fb1c83e4085469747387
dnisaev <dnisaev@yandex.ru>
2024-05-06 18:00:19 +0300

    add store

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { BrowserRouter } from 'react-router-dom'
-
 import Logo from '@/assets/icons/Logo'
 import { UserDropDown } from '@/components/layout/UserDropDown'
 import { Header } from '@/components/ui/Header/Header'
+import { Decks } from '@/pages/decks'
 
 export function App() {
   return (
-    <BrowserRouter>
+    <div>
       <Header email={'frontend-dev@gmail.com'} isLoggedIn logout={() => {}} name={'Ramin'} />
       <Logo height={'36px'} width={'157px'} />
       <div style={{ margin: ' 0 auto', maxWidth: '300px' }}>
         <UserDropDown email={'frontend-dev@gmail.com'} logout={() => {}} name={'Ramin'} />
       </div>
-    </BrowserRouter>
+      <Decks />
+    </div>
   )
 }
